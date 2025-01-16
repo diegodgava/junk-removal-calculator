341130067a79071a19c7c578f7cb071fd117aed0
Diego Gava <diego.gava15@gmail.com>
2025-01-16 16:28:23 -0300

    Initial commit 2

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Head from "next/head"; // Importa o componente Head
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +15,12 @@ const geistMono = Geist_Mono({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        {/* Define o título da aba */}
+        <title>Junk Calculator</title>
+        {/* Adiciona o favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
